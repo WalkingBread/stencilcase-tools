5b3fdaa48b80ab6278ade51f8d543ecc54197d30
Mateusz Skorupiński <34984014+WalkingBread@users.noreply.github.com>
2022-09-15 16:35:13 +0200

    banana

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-from layer import Layer
+from canvas import Layer
 from PIL import Image
 
 
 if __name__ == '__main__':
-    pass
+    img = Image.open('samples/walter.jpg')
+    pixels = img.load()
+    layer = Layer(image=img)
+    layer = layer.limit_colors([(0, 0, 0, 255), (100, 100, 100, 255), (255, 255, 255, 255)])
+    layer1 = layer.extract_color((0, 0, 0, 255))
+    layer2 = layer.extract_color((100, 100, 100, 255))
+    layer3 = layer.extract_color((255, 255, 255, 255))
+    #layer = layer.get_edges()
+    layer1.save('samples/woman1.png')
+    layer2.save('samples/woman2.png')
+    layer3.save('samples/woman3.png')
